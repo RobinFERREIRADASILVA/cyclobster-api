6ede6747da81b11ae2a7c3c7bfb0a52fe4f2882c
RobinFERREIRADASILVA <ferreiradasilvarobin@gmail.com>
2021-10-09 11:13:16 +0200

    trying db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize("cyclobster", "root", "root", {
+module.exports = new Sequelize("cyclobster", "robin", "pantyr77220", {
     dialect: "mysql",
     host: "localhost"
 });
